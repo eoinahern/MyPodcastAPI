d80fc2ecce59eb9ff5745822997df0143a1531e3
eoinahern <eoin.ahern@mycit.ie>
2017-09-20 13:42:46 +0100

    added some model structs

---
+++ models/Models.go
@@ -0,0 +1,23 @@
+package models
+
+type User struct {
+	UserName string
+	Password []byte
+	Podcasts []Podcast
+}
+
+type Podcast struct {
+	Username string
+	Icon     string
+	Name     string //name of podcast
+	Details  string //info about the podcast
+	Episodes []Episode
+}
+
+type Episode struct {
+	Episode   int32
+	Created   string
+	Location  string
+	Downloads int32
+	Details   string
+}
