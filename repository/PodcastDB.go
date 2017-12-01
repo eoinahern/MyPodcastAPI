6d1b9817763c7a64e59c3c5270d610b15a82e46a
eoinahern <eoin.ahern@mycit.ie>
2017-12-01 15:15:51 +0000

    update small

--- repository/PodcastDB.go
+++ repository/PodcastDB.go
@@ -1,5 +1,15 @@
 package repository
 
+import "my_podcast_api/models"
+
 type PodcastDB struct {
 	*DB
+}
+
+func (DB *PodcastDB) getAll() {
+
+}
+
+func (DB *PodcastDB) getPodcast(id string) *models.Podcast {
+	return nil
 }
