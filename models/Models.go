80c3c006392310a13d059db12d8b8264cc949572
eoinahern <eoin.ahern@mycit.ie>
2017-12-02 14:04:36 +0000

    updated tables

--- models/Models.go
+++ models/Models.go
@@ -11,7 +11,7 @@ type User struct {
 	UserName string    `json: "username" gorm: "type:TEXT; primary_key; not null; unique"`
 	Verified bool      `json: "verified" gorm: "type : "BOOLEAN" `
 	Password string    `json: "password" gorm: "type: TEXT"`
-	Podcasts []Podcast `json: "podcasts" gorm: "ForeignKey:UserName"`
+	Podcasts []Podcast `json: "podcasts" gorm: "ForeignKey:UserEmail;AssociationForeignKey:UserName"`
 }
 
 type Message struct {
@@ -19,17 +19,19 @@ type Message struct {
 }
 
 type Podcast struct {
-	Icon     string    `json: "icon"`
-	Name     string    `json: "name"`     //name of podcast
-	Details  string    `json : "details"` //info about the podcast
-	Episodes []Episode `json: "episodes"`
+	UserEmail string    `json: "useremail" gorm: "type:TEXT"`
+	Icon      string    `json: "icon"   gorm: "type:TEXT"`
+	Name      string    `json: "name" gorm: "type: TEXT"`    //name of podcast
+	Details   string    `json : "details" gorm: "type:TEXT"` //info about the podcast
+	Episodes  []Episode `json: "episodes"  gorm: "ForeignKey:UserID; AssociationForeignKey:UserEmail"`
 }
 
 type Episode struct {
-	Episode   int32  `json: "episode"`
-	Created   string `json: "created"`
-	Updated   string `json: "updated"`
-	URL       string `json: "url"`
-	Downloads int32  `json: downloads`
-	Details   string `json: "details"`
+	UserID    string `json: "userid"  gorm: "type:Text"`
+	Episode   int32  `json: "episode" gorm: "type: INTEGER"`
+	Created   string `json: "created" gorm: "type: TEXT"`
+	Updated   string `json: "updated" gorm: "type: TEXT"`
+	URL       string `json: "url" gorm: "type: TEXT"`
+	Downloads int32  `json: "downloads" gorm: "type: INTEGER"`
+	Details   string `json: "details" gorm: "type: TEXT"`
 }
