d8f596393dcbd1eee4391ba599c8d93e391118c5
eoinahern <eoin.ahern@mycit.ie>
2017-12-05 15:39:30 +0000

    added methods to PasswordEncryptionUtil

--- util/PasswordEncryptionUtil.go
+++ util/PasswordEncryptionUtil.go
@@ -1,20 +1,30 @@
 package util
 
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type PasswordEncryptUtil struct {
-
 }
 
-func (p *PasswordEncryptUtil) encrypt() string {
-    return ""
+func (p *PasswordEncryptUtil) encrypt(password string) string {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return hash
 }
 
+func (p *PasswordEncryptUtil) checkSame(DBpassword string, sentPassword string) bool {
 
-func (p *PasswordEncryptUtil) decrypt() string {
-  return ""
-}
-
-
-func (p *PasswordEncryptUtil) checkSame() bool {
-  return true
+	if err := bcrypt.CompareHashAndPassword(DBpassword, []byte(sentPassword)); err != nil {
+		return false
+	} else {
+		return true
+	}
 }
