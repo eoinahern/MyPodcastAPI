2fe4f54ebd1fe6943deb50c40b997352f334d0af
eoinahern <eoin.ahern@mycit.ie>
2018-02-08 21:37:45 +0000

    fixed some lint isses

--- repository/UserDB.go
+++ repository/UserDB.go
@@ -9,13 +9,15 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+//UserDB : used to do CRUD operations on the users DB table
 type UserDB struct {
 	*gorm.DB
 }
 
+//CheckExist : check user exists in table by users email address
 func (DB *UserDB) CheckExist(email string) bool {
 
-	var count int = 0
+	var count int
 	DB.Model(&models.User{}).Where("user_name = ?", email).Count(&count)
 
 	if count >= 1 {
@@ -25,9 +27,10 @@ func (DB *UserDB) CheckExist(email string) bool {
 	return false
 }
 
+//ValidateUserPlusRegToken : check if user with specific registration exists in table
 func (DB *UserDB) ValidateUserPlusRegToken(email string, regToken string) bool {
 
-	var count int = 0
+	var count int
 	DB.Model(&models.User{}).Where("user_name = ? AND reg_token = ?", email, regToken).Count(&count)
 
 	if count == 1 {
@@ -37,6 +40,7 @@ func (DB *UserDB) ValidateUserPlusRegToken(email string, regToken string) bool {
 	return false
 }
 
+//SetVerified : set user with specific token and email to be verified in table
 func (DB *UserDB) SetVerified(username string, token string) {
 
 	var user models.User
@@ -50,6 +54,7 @@ func (DB *UserDB) SetVerified(username string, token string) {
 
 }
 
+//ValidatePasswordAndUser : check user exists with specific password
 func (DB *UserDB) ValidatePasswordAndUser(email string, password string) bool {
 
 	var user models.User
@@ -64,6 +69,7 @@ func (DB *UserDB) ValidatePasswordAndUser(email string, password string) bool {
 	}
 }
 
+//Insert : Add new user to the users table
 func (DB *UserDB) Insert(user *models.User) {
 	DB.Save(user)
 }
@@ -75,17 +81,6 @@ func (DB *UserDB) GetUser(email string) models.User {
 	return user
 }
 
-func (DB *UserDB) GetAll() {
-
-}
-
-func (DB *UserDB) update(email string) {
-
-}
-
 func (DB *UserDB) delete(email string) bool {
-
-	//delete user if worked return true
 	return true
-
 }
