1c3d975a04c196560e6ae37a356f48776df31757
eoinahern <eoin.ahern@mycit.ie>
2018-02-12 12:44:21 +0000

    added smtpConfig.json

--- util/RegistrationMailHelper.go
+++ util/RegistrationMailHelper.go
@@ -1,19 +1,29 @@
 package util
 
+import (
+	"net/smtp"
+)
+
+const host string = "smtp.gmail.com"
+
 type Mail struct {
 	SenderId     string
 	ToId         string
 	Subject      string
 	BodyLocation string
+	BodyParams   map[string]string
 }
 
 func (m *Mail) SendMail() {
 
-	//setup
-	//send a mail
+	auth := smtp.PlainAuth("", username, "hellothere123", host)
 
 }
 
 func (m *Mail) BuildMail() string {
 	return ""
+}
+
+func (m *Mail) ConstructTemplate() {
+
 }
